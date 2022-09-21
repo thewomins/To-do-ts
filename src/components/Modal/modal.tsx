4b347f466160569628021cba2e46aefeb6fde5de
Felipe Urrutia <thewomins@gmail.com>
2022-09-21 14:33:17 -0300

    Add small and show properties

--- src/components/Modal/modal.tsx
+++ src/components/Modal/modal.tsx
@@ -5,12 +5,24 @@ type props={
   children :any
   title? : string
   onClickOutside?:any
+  style?:any
+  show:boolean
+  small?:boolean
 }
 
-const Modal: React.FC<props> = ({onClickOutside, title, children}) => {
+const Modal: React.FC<props> = ({onClickOutside, title, children,small,style,show}) => {
   return (
-    <div className="outsideModal" onClick={() => onClickOutside()}>
-      <div onClick={e => e.stopPropagation()} className="containerModal">
+    <div 
+        className="outsideModal" 
+        onClick={() => onClickOutside()}
+        style={{...style,display:show? "flex":"none"}}
+        hidden={!show}
+      >
+      <div 
+        onClick={e => e.stopPropagation()} 
+        className="containerModal"
+        style={small? {height: "auto",width: "30%"}:{}}
+        >
         <div className="titleModal">
           <h2>{title}</h2>
         </div>
