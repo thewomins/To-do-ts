4195d90921bf5847dfc681b705413005520fc8a2
Felipe Urrutia <thewomins@gmail.com>
2022-10-02 10:37:26 -0300

    Add edit title logic

--- src/screens/modalTodo/modalTodo.tsx
+++ src/screens/modalTodo/modalTodo.tsx
@@ -6,7 +6,7 @@ import Button from "src/components/Button/button";
 import Input from "src/components/Input/input";
 import { useTodo } from "src/hooks/Todo";
 import TaskCardMenu from "src/components/TaskCardWithMenu/taskCardMenu";
-import {MdAdd,MdCancel} from "react-icons/md";
+import {MdAdd,MdCancel,MdDeleteForever,MdEdit,} from "react-icons/md";
 
 type props = {
   show: boolean;
@@ -21,6 +21,17 @@ const ModalTodo: React.FC<props> = ({setShow, show, idTodo}) => {
   const {Todos, dispatch} = useTodo();
   const todoPos = Todos.todoItems.findIndex(todo => todo.id === idTodo);
   console.info("totalpos",todoPos,"todos", Todos,"id",idTodo);
+  //title handler
+  const [editTitleMode,setEditTitleMode] = useState(false);
+  const [title,setTitle] = useState("");
+
+  useEffect(() =>{
+    console.log(title);
+  },[title]);
+
+  const onChangeTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
 
   const onClickOutside = () => {
     setShow(false);
@@ -48,10 +59,69 @@ const ModalTodo: React.FC<props> = ({setShow, show, idTodo}) => {
     setAddTaskClicked(false);
   });
 
+  const onClickEditTitle= ((titulo:string) => {
+    setEditTitleMode(true);
+    setTitle(titulo);
+    console.log("edit",titulo);
+  });
+
+  const stopEditingTitle= (() => {
+    setEditTitleMode(false);
+    setTitle("");
+  });
   
+  const onClickAddTitle= ((TodoId:string) => {
+    dispatch({type:"changeTitle", idTodo:idTodo, title:title});
+    console.log("changeTitle",TodoId, title);
+    stopEditingTitle();
+  });
   
   return (
-    <Modal title={Todos.todoItems[todoPos].nombre} onClickOutside={onClickOutside} show={show}>
+    <Modal onClickOutside={onClickOutside} show={show}>
+      {!editTitleMode ?
+      <div className="titleModalDetails">
+        <h2 style={{alignSelf:"center"}}>{Todos.todoItems[todoPos].nombre}</h2>
+        <div
+          className="iconContainerTitle"
+          >
+          <div
+            className="iconContainerMargin"
+            onClick={()=>onClickEditTitle(Todos.todoItems[todoPos].nombre)}
+          >
+            <MdEdit className="icon"/>
+          </div>
+        </div>
+      </div>
+      :<div style={{display:"flex",height:"60px", marginTop:"20px"}}>
+        <Input
+          style={{margin: 5,width:"100%"}}
+          id={"addeded"}
+          textoInput="Titulo"
+          type="text"
+          color="primary"
+          onChange={onChangeTitle}
+          value={title}
+          >
+          <div className="inputHandlerContainer">
+            <div 
+              className="iconContainerL" 
+              style={{paddingLeft: "5px"}}
+              onClick={()=>onClickAddTitle(Todos.todoItems[todoPos].id)}
+              >
+              <MdAdd className="icon"/>
+            </div>
+            <div className="divisor"/>
+            <div 
+              className="iconContainerR"
+              style={{paddingRight: "5px"}}
+              onClick={()=>stopEditingTitle()}
+              >
+              <MdCancel className="icon"/>
+            </div>
+          </div>
+        </Input>
+      </div>
+      }
       <div className="containerDescription">
         <div className="containerTasks">
           {Todos?.todoItems[todoPos].list.map((task, index) => (
