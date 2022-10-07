acbd87a9240c67ef56d1822c0f8e29f79e88223b
Felipe Urrutia <thewomins@gmail.com>
2022-10-07 11:52:39 -0300

    Apply switch component

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { MdModeNight } from "react-icons/md";
+import Switch from "../Switch/switch";
 import "./menu.css";
 
-const Menu: React.FC = () => {
+type props = {
+  state:boolean
+  onChangeCheck:any
+}
+
+const Menu: React.FC<props> = ({state, onChangeCheck}) => {
   return (
     <nav className="container">
       <h3>To-do</h3>
+      <div className="nightModeContainer">
+        <MdModeNight className="iconNight"/>
+        <Switch
+          state={state}
+          onChangeCheck={onChangeCheck}
+        />
+      </div>
+      
     </nav>
   );
 };
