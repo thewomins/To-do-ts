50ea56c304fcb0ac81e4746e5deb3ebf0b7c60ed
Felipe Urrutia <thewomins@gmail.com>
2022-10-24 11:57:34 -0300

    Fix check state not updating and add types

--- src/components/Switch/switch.tsx
+++ src/components/Switch/switch.tsx
@@ -2,17 +2,17 @@ import React from "react";
 import "./switch.css";
 
 type props = {
-  state:boolean
-  onChangeCheck:any
-}
+  state: boolean;
+  onChangeCheck: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
 
 const Switch: React.FC<props> = ({state, onChangeCheck}) => {
   return (
     <label className="switch">
-      <input 
-        type={"checkbox"} 
-        onChange={(e:any)=>onChangeCheck(e)}
-        defaultChecked={state}
+      <input
+        type={"checkbox"}
+        onChange={e => onChangeCheck(e)}
+        checked={state}
       />
       <span className="slider"></span>
     </label>
